128959ce16a72169b0ae2f9c5f24171e22e43938
vaba1010 <valentin.baranov@netcracker.com>
2017-01-30 18:24:39 +0400

    Log4j added in test mode issue #10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 import controller.Controller;
 import controller.SwingApplicationController;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 public class Main {
 
+    private static final Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
+        LOGGER.error("Starting application (logging test)...");
         Controller appController = SwingApplicationController.getInstance();
         appController.startApplication();
     }
